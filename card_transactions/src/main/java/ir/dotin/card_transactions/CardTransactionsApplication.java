8ef1f246573a904f00a69f0f029dac2f6a1aaed6
mehdi_mh93 <mahdimirdar48@yahoo.com>
2020-10-25 00:23:41 +0330

    initial commit

---
+++ card_transactions/src/main/java/ir/dotin/card_transactions/CardTransactionsApplication.java
@@ -0,0 +1,14 @@
+package ir.dotin.card_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardTransactionsApplication.class, args);
+        System.out.println("test");
+    }
+
+}
---
+++ card_transactions/src/test/java/ir/dotin/card_transactions/CardTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.dotin.card_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
